8f19eddb27a9c9ab81db8a7480e778c11e24349e
Zarillion <zarillion@zarillion.com>
2025-03-06 20:45:44 -0600

    Update lockenstock

--- undermine/5-lockenstock-init.lua
+++ undermine/5-lockenstock-init.lua
@@ -2,26 +2,26 @@ local aura_env = aura_env
 
 aura_env.soakers = {}
 
-local function Emit (message, target)
+local function Emit(message, target)
     if target then
-        C_ChatInfo.SendAddonMessage('TMDMv1', message, 'WHISPER', target)
+        C_ChatInfo.SendAddonMessage("TMDMv1", message, "WHISPER", target)
     else
-        C_ChatInfo.SendAddonMessage('TMDMv1', message, 'RAID')
+        C_ChatInfo.SendAddonMessage("TMDMv1", message, "RAID")
     end
 end
 
-local function UnitDebuff (unit, spell)
+local function UnitDebuff(unit, spell)
     return AuraUtil.FindAuraByName(spell, unit, "HARMFUL")
 end
 
 aura_env.MRT = function()
-    if C_AddOns.IsAddOnLoaded("MRT") and _G.VExRT.Note.Text1 then
+    if C_AddOns.IsAddOnLoaded("MRT") and VExRT.Note.Text1 then
         aura_env.soakers = {}
 
         local list = false
-        local text = _G.VExRT.Note.Text1
+        local text = VExRT.Note.Text1
 
-        for line in text:gmatch('[^\r\n]+') do
+        for line in text:gmatch("[^\r\n]+") do
             line = strtrim(line)
             if strlower(line) == "minestart" then
                 list = true
@@ -39,16 +39,16 @@ end
 local MESSAGE = {
     "m=|T4624638:0|t SOAK MINE |T4624638:0|t",
     "c=SAY:Here I go soakin' again!",
-    "s=bikehorn"
+    "s=bikehorn",
 }
 
 local SANARC_MESSAGE = {
     "m=|T4624638:0|t SNIFF FOOT-BLASTER |T4624638:0|t",
     "c=YELL:Foot-Blaster?!? It has foot in the name, I'm sniffing it!",
-    "s=moan"
+    "s=moan",
 }
 
-aura_env.AssignSoaker = function ()
+aura_env.AssignSoaker = function()
     for _, name in ipairs(aura_env.soakers) do
         if not (UnitIsDead(name) or UnitDebuff(name, "Unstable Shrapnel")) then
             if name == "Sanarc" then
