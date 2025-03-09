ee0949a3ac196e448d32779e4ed9b01d3b6f9996
Zarillion <zarillion@zarillion.com>
2025-03-09 15:54:34 -0500

    Update bandit

--- Raids/34 - Liberation of Undermine/6-bandit-init.lua
+++ Raids/34 - Liberation of Undermine/6-bandit-init.lua
@@ -2,6 +2,16 @@ local aura_env = aura_env
 
 aura_env.combo = 0
 
+aura_env.REWARDS = {
+    461101, -- Gold + Shock
+    461389, -- Gold + Flame
+    461395, -- Gold + Bomb
+    461091, -- Shock + Bomb
+    461176, -- Flame + Bomb
+    461083, -- Shock + Flame
+    465309, -- Cheat to Win!
+}
+
 local function Emit(message, target)
     if target then
         C_ChatInfo.SendAddonMessage("TMDMv1", message, "WHISPER", target)
@@ -20,7 +30,11 @@ local COMBOS = {
 }
 
 aura_env.DisplayCombo = function(i)
-    local combo = COMBOS[i] or "WE'RE FUCKED?!"
-    local message = "m2=" .. combo .. ";d=30"
+    local combo = COMBOS[i] or "{skull} PUSH P2 OR DIE {skull}"
+    local message = "m2=" .. combo .. ";d=30;b=::30:1:1:.4"
     Emit(message)
+end
+
+aura_env.StopDisplayCombo = function()
+    Emit("b=;m2=")
 end
--- Raids/34 - Liberation of Undermine/6-bandit-trigger.lua
+++ Raids/34 - Liberation of Undermine/6-bandit-trigger.lua
@@ -3,12 +3,19 @@ trigger = function(event, ...)
     if event == "ENCOUNTER_START" then
         aura_env.combo = 0
     elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        local _, message, _, _, _, _, _, _, destName, _, _, spellID = ...
+        local _, message, _, _, srcName, _, _, _, destName, _, _, spellID = ...
         if message == "SPELL_AURA_APPLIED" and spellID == 471927 then -- Withering Flame
             C_ChatInfo.SendAddonMessage("TMDMv1", "m1=GET OUT!;s=bikehorn", "WHISPER", destName)
-        elseif message == "SPELL_CAST_SUCCESS" then -- Spin To Win!
+        elseif message == "SPELL_CAST_SUCCESS" and spellID == 461060 then -- Spin To Win!
             aura_env.combo = aura_env.combo + 1
             aura_env.DisplayCombo(aura_env.combo)
+        elseif message == "SPELL_CAST_START" and srcName == "One-Armed Bandit" then -- Reward casts
+            for _, spell in ipairs(aura_env.REWARDS) do
+                if spell == spellID then
+                    aura_env.StopDisplayCombo()
+                    break
+                end
+            end
         end
     end
 end
